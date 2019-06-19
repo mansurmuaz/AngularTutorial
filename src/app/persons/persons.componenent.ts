d3f17acdf08fc30d3324fabaefe563d3300697b0
Mansur Muaz Ekici <mmuazekici@gmail.com>
2019-06-19 10:48:22 +0200

    first component

---
+++ src/app/persons/persons.componenent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-persons',
+    templateUrl: './persons.component.html',
+    styleUrls: ['./persons.component.css']
+})
+
+export class PersonsComponent {}
